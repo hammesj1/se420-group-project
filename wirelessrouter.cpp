200533edede6514c6356058f62c3af013fd11a11
CristianERAU <garcic14@my.erau.edu>
2018-04-10 15:24:32 -0700

    Wireless Router Module Improvements
    Optomized Wireless Router module to include only intented and implemented features.

--- wirelessrouter.cpp
+++ wirelessrouter.cpp
@@ -1,17 +1,18 @@
 ///////////////////////////////
-// CS225 Spring 2016 //////////
+// SQA Project 2018 ///////////
 // Cristian Garcia ////////////
+// Josh Hammes ////////////////
 ///////////////////////////////
 
-// Include Derectives /////////
+// Include Derectives
 #include "wirelessrouter.hpp"
 #include <iostream>
 #include <string>
 
-// Implementation Section: ///////////////////////////////////////////////////////////
+// Implementation Section:
 int WirelessRouter::num_alive=0;
 
-// Default Create and Keep Track Number of Packet Switches ///////////////////////////
+// Default Create and Keep Track Number of Wireless Routers
 WirelessRouter::WirelessRouter()
 {
    packet_size = 0;
@@ -21,22 +22,22 @@ WirelessRouter::WirelessRouter()
    //net_ptr=NULL;
 }
 
-// Default Set Array of Packet Switch Objects ////////////////////////////////////////
+// Default Set Array of Wireless Routers Objects
 WirelessRouter::WirelessRouter(int new_packet_size, std::string new_ieee_std, double new_data_rate)
 {
    packet_size = new_packet_size;
    ieee_std = new_ieee_std;
    data_rate = new_data_rate;
    num_alive++;
-} 
+}
 
-// Destroy and Delete Track of Number of Packet Switches /////////////////////////////
+// Destroy and Delete Track of Number of Wireless Routers
 WirelessRouter::~WirelessRouter()
 {
    num_alive--;
-} 
+}
 
-// Check if Packet Sewitch is Empty or Defined as Default ////////////////////////////
+// Check if Wireless Router is Empty or Defined as Default
 int WirelessRouter::isempty()
 {
   if(packet_size == 0 && ieee_std == "" && data_rate == 0.0)
@@ -45,41 +46,35 @@ int WirelessRouter::isempty()
    return 0;
 }
 
-// Receive Information from Main and Set Packet Switch Object Number of Ports ////////
+// Receive Information from Main and Set Wireless Router Object Number of Ports
 int WirelessRouter::set_packet_size(int new_packet_size)
 {
    packet_size = new_packet_size;
    return 0;
 }
 
-// Receive Information from Main and Set Packet Switch Object Vendor /////////////////
+// Receive Information from Main and Set Wireless Router Object Vendor
 int WirelessRouter::set_ieee_std(std::string new_ieee_std)
 {
    ieee_std = new_ieee_std;
    return 0;
 }
 
-// Receive Information from Main and Set Packet Switch Object Per Port Speed /////////
+// Receive Information from Main and Set Wireless Router Object Per Port Speed
 double WirelessRouter::set_data_rate(double new_data_rate)
 {
    data_rate = new_data_rate;
    return 0;
 }
 
-double WirelessRouter::compute_packetspersec()///double pps, int num_ports, int packet_size) //////////////////     look!!!!!
+// Compute Pacckets Per Second for Wireless Router
+double WirelessRouter::compute_packetspersec()
 {
    packets_per_sec = (data_rate / (8 * packet_size));
    return 0;
 }
 
-////int PacketSwitch::set_network_ptr(Network* new_net_ptr)
-////{
-////   net_ptr = new_net_ptr;
-////   return 0;
-////}
-
-
-// Set All Packet Switch Object Data to Blank or Default /////////////////////////////
+// Set All Wireless Router Object Data to Blank or Default
 int WirelessRouter::set_blank()
 {
    packet_size = 0;
@@ -88,20 +83,20 @@ int WirelessRouter::set_blank()
    return 0;
 }
 
-// Retrive Number of Alive Packet Switch Objects /////////////////////////////////////
+// Retrive Number of Alive Wireless Router Objects
 int WirelessRouter::get_num_alive()
 {
    return num_alive;
 }
 
-// Determine and Display Packet Switch Array Memory Usage ////////////////////////////
+// Determine and Display Wireless Router Array Memory Usage
 int WirelessRouter::display_mem_usage()
 {
   std::cout << "Program Memory Usage: " << get_num_alive()* sizeof(WirelessRouter) << std::endl;
   return 0;
 }
 
-//Retrieve All Packet Switch Object Information /////////////////////////////////////
+//Retrieve All Wireless Router Object Information
 int WirelessRouter::toCout()
 {
    std::cout << "Wireless Router Packet Size          : " << get_packet_size() << std::endl;
@@ -110,29 +105,11 @@ int WirelessRouter::toCout()
    return 0;
 }
 
-////Component* PacketSwitch::get_component(int index){return &loc_component_array[index];}
-
 ostream &operator<<( ostream &output, const WirelessRouter &Wr)
 {
    Wr.PacketSwitch::toCout();
    output << "Wireless Router Packet Size          : " << Wr.get_packet_size() << std::endl
           << "Wireless Router IEEE                 : " << Wr.get_ieee_std() << std::endl
           << "Wireless Router Data Rate            : " << Wr.get_data_rate() << std::endl;
-
-          ////if(Ps.net_ptr != NULL)
-          ////{ 
-          ////  output << *(Ps.net_ptr);
-          ////}
-          ////else 
-          ////{
-          ////  output<<"Is not Network"<< std::endl;
-          ////}
-          ////for(int i = 0; i < 20 ; i++)
-          ////{
-          ////  if(Ps.loc_component_array[i].isempty()){}
-          ////  else
-          ////  output << Ps.loc_component_array[i] << std::endl;
-          ////}
-
    return output;
 };
--- wirelessrouter.hpp
+++ wirelessrouter.hpp
@@ -1,51 +1,51 @@
 ///////////////////////////////
-// CS225 Spring 2016 //////////
+// SQA Project 2018 ///////////
 // Cristian Garcia ////////////
+// Josh Hammes ////////////////
 ///////////////////////////////
 
 ///////////////////////////////
 #ifndef WIRELESSROUTER_HPP_
 #define WIRELESSROUTER_HPP_
-// Include Directives /////////
+
+// Include Directives
 #include <iostream>
 #include <string>
 #include "packetswitch.hpp"
 using std::ostream;
 
-// Decleration Section: //////////////////////////////////////////////////////////////
+// Decleration Section:
 class WirelessRouter:public PacketSwitch
 {
    private:
-     // Data Hiding //////////////////////////////////////////////////////////////////
+     // Data Hiding
      static int num_alive;
      int packet_size;
      std::string ieee_std;
      double data_rate;
 
    public:
-     // Constructor //////////////////////////////////////////////////////////////////
+     // Constructor
      WirelessRouter();
-
-     /////////////////////////////////////////////////////////////////////////////////
      WirelessRouter(int, std::string, double);
 
-     // Destructor ///////////////////////////////////////////////////////////////////
+     // Destructor
      ~WirelessRouter();
 
-     // Mutators ////////////////////////////////////////////
+     // Mutators
      int set_packet_size(int);
      int set_ieee_std(std::string);
      double set_data_rate(double);
      double compute_packetspersec();
      int set_blank();
-     
-     // Accessors ///////////////////////////////////////////
+
+     // Accessors
      static int get_num_alive();
      int get_packet_size()const{return packet_size;}
      std::string get_ieee_std()const{return ieee_std;}
      double get_data_rate()const{return data_rate;}
 
-     // Helpers //////////////////////////////////////////////////////////////////////
+     // Helpers
      int isempty();
      int display_mem_usage();
      int toCout();
